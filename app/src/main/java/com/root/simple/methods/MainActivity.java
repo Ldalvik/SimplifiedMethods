2a535f655abb4fb3c7f11b1d2c8e5419cda86752
ImRooot <imnotsuda@hotmail.com>
2017-03-02 20:40:29 -0500

    Initial commit

---
+++ app/src/main/java/com/root/simple/methods/MainActivity.java
@@ -0,0 +1,14 @@
+package com.root.simple.methods;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
